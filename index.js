47672c9f98eb2e42772dbd85ce42f065a0d1c7a6
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-04-17 18:40:02 -0400

    - move sub-folder imports to index

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var Bleacon = require('./lib/bleacon');
 
+var bleacon = new Bleacon();
 
-module.exports = new Bleacon();
+bleacon.BleuStation = require('./bleu-station/bleu-station');
+bleacon.Estimote = require('./estimote/estimote');
+bleacon.EstimoteSticker = require('./estimote-sticker/estimote-sticker');
+
+module.exports = bleacon;
